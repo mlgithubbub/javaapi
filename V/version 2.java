f9c38b65fd098b710e6182b74052bdaa66693690
morganlinn <to.morganlinn@gmail.com>
2024-12-07 20:19:48 +0100

    commit v

---
+++ V/version 2.java
@@ -0,0 +1,11 @@
+package jdk.internal.jshell.tool.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "20+36-2344" },
+            { "jdk", "20" },
+            { "release", "20" },
+        };
+    }
+}
---
+++ V/version.java
@@ -0,0 +1,11 @@
+package com.sun.tools.javac.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "20+36-2344" },
+            { "jdk", "20" },
+            { "release", "20" },
+        };
+    }
+}
